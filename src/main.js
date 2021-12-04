19e46ba139df9728116223890cda4ff859410195
Jason Bedini <jasonbedini@gmail.com>
2021-12-04 15:32:35 -0800

    Local Storage is implemented and working fully

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './main.css';
 import { project, todo } from './classes.js';
 let hash = require('hash.js');
 import * as storage from './local-storage.js';
-import {createProjectHtml} from './DOM-manipulation.js'
+import {createProjectHtml, addToProject} from './DOM-manipulation.js'
 import {projects, todos} from './modules.js';
 import {createNewProject} from './createProjects.js';
 import {addProjectEventListener} from './project-EventListeners.js'
@@ -14,22 +14,45 @@ import {addProjectEventListener} from './project-EventListeners.js'
 // check if local storage contains projects or to-do's
 if(storage.checkLocalStorage()) {
     console.log(localStorage);
+
+    // set up the add a project button 
+    addProjectEventListener();
+
+    // retrieve the projects and their todos
     storage.extractArraysLocalStorage();
-    console.log(todos.todoArray);
-    console.log(projects.projectsArray);
-    localStorage.clear();
+
+    // iterate through the projects array and append them to the page
+    for(let i = 0; i < projects.projectsArray.length; i++) {
+        let currentProject = projects.projectsArray[i];
+        createProjectHtml(currentProject);
+        
+        // get key of current project
+        let key = currentProject.key;
+
+        // use key to filter out the todos with the corresponding key, match them to their project and append them
+        let filteredToDos = todos.todoArray.filter(todo => todo.key === key);
+        for(let i = 0; i < filteredToDos.length; i++) {
+            addToProject(filteredToDos[i]);
+        };
+    };
+
+    
 }
 else {
-    // create a project object for general tasks, obtain hash first
+    // create a project object for general tasks, obtain hash first, check if hash starts with a number
 
+    // obtain hash of project title, then change first value to an int
     let hashCode = hash.sha256().update('General Tasks').digest('hex');
     let hashSplit = hashCode.split('');
     let trueValue = parseInt(hashSplit[0]);
+
+    // if the int is not between 0 and 9 inclusive, then the int corresponds to a letter
     if(0 <= trueValue <=9) {
         hashSplit.shift();
         hashSplit.unshift('a');
         hashCode = hashSplit.join('');
     };
+
     let general = createNewProject('General Tasks', 'N/A', hashCode);
     
     // display the general project on the webpage
